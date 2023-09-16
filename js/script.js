ba73a929664821852942120fa59f1a5d3a40b4d2
Fr13nds07860376 <119647325+Fr13nds07860376@users.noreply.github.com>
2023-09-15 22:42:42 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,36 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navlist.classList.toggle('active');
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
 };
 
 window.onscroll = () => {
-	menu.classList.remove('bx-x');
-	navlist.classList.remove('active');
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
 };
 
-const sr = ScrollReveal ({
-	distance: '45px',
-	duration: 2700,
-	reset: true
-})
+document.addEventListener('DOMContentLoaded', function() {
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    });
+});
 
-sr.reveal('.home-text',{delay:350, origin:'left'})
-sr.reveal('.home-img',{delay:350, origin:'right'})
+const sr = ScrollReveal({
+    distance: '45px',
+    duration: 2700,
+    reset: true
+});
 
-sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.home-text', { delay: 350, origin: 'left' });
+sr.reveal('.home-img', { delay: 350, origin: 'right' });
 
+sr.reveal('.sub-service, .about, .portfolio, .service, .cta, .contact', { delay: 200, origin: 'bottom' });
